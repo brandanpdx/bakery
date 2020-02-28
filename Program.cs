25058006c2b3cc4088b09a99788ace5c9af363e6
Brandan Sayarath <brandan@brandan.tech>
2020-02-28 08:54:21 -0800

    add initial Welcome method to be called at start of program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
 using System;
+using Model; 
+
+public class Program
+{
+  public static void Main()
+  {
+    Welcome();
+  }
+
+  public static void Welcome()
+  {
+    Console.WriteLine("-------------------------------------------------------------------------------");
+    Console.WriteLine("Welcome to Pierre's Bakery! Bread is $5 per loaf.  Pastries are $2 each.");
+    Console.WriteLine("TODAY'S SPECIALS: Buy 2 loaves of bread, get 1 free! Buy 3 pastries for $5!");
+    Console.WriteLine("-------------------------------------------------------------------------------");
+  }
+}
