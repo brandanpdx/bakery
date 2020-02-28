8709a046078451ee7bd61b91a5f7585fafe418cb
Brandan Sayarath <brandan@brandan.tech>
2020-02-28 13:01:54 -0800

    work on function to add sale prices

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -4,12 +4,13 @@ namespace Model
   {
     public static int BreadPrice { get; set; }
     public static int BreadQuantity { get; set; }
+    public static int BreadTotal { get; set; }
 
     public static void BreadCalc(int qty)
     {
       BreadQuantity = qty;
       BreadPrice = 5 * qty;
-
+      BreadTotal = (BreadPrice / 3) * 2 + BreadPrice % 3;
     }
 
   }
