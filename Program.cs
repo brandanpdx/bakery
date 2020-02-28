325dcdb84a090e2ae0eedcb1b01a2001958a0934
Brandan Sayarath <brandan@brandan.tech>
2020-02-28 10:01:38 -0800

    add additional prompts to gather user inputs

--- Program.cs
+++ Program.cs
@@ -6,13 +6,32 @@ public class Program
   public static void Main()
   {
     Welcome();
+    BreadQuantity();
+    PastryQuantity();
   }
 
   public static void Welcome()
   {
-    Console.WriteLine("-------------------------------------------------------------------------------");
+    Console.WriteLine("--------------------------------------------------------------------------");
     Console.WriteLine("Welcome to Pierre's Bakery! Bread is $5 per loaf.  Pastries are $2 each.");
     Console.WriteLine("TODAY'S SPECIALS: Buy 2 loaves of bread, get 1 free! Buy 3 pastries for $5!");
-    Console.WriteLine("-------------------------------------------------------------------------------");
+    Console.WriteLine("--------------------------------------------------------------------------");
   }
+  public static void BreadQuantity()
+  {
+    Console.WriteLine("How many loaves of bread do you need today?");
+    string getBread = Console.ReadLine();
+    int breadQuantity = int.Parse(getBread);
+    Console.WriteLine(breadQuantity + " loaves of bread added to cart!");
+    Console.WriteLine("--------------------------------------------------------------------------");
+  }
+  public static void PastryQuantity()
+  {
+    Console.WriteLine("How many pastries do you need today?");
+    string getPastry = Console.ReadLine();
+    int pastryQuantity = int.Parse(getPastry);
+    Console.WriteLine(pastryQuantity + " pastries added to cart!");
+    Console.WriteLine("--------------------------------------------------------------------------");
+  }
+  
 }
