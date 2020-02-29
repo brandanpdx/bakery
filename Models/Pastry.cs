a8cfa1a7068770343927864ce3ecc66e6c12f824
Brandan Sayarath <brandan@brandan.tech>
2020-02-28 18:19:24 -0800

    add pastry price discount

--- Models/Pastry.cs
+++ Models/Pastry.cs
@@ -3,19 +3,20 @@ namespace Model
   public class Pastry
   {
     public static int PastryPrice { get; set; }
-    public static int PastryQuantity {get; set; }
+    public static int PastryQuantity { get; set; }
+    public static int groupedPastry;
+    public static int individualPastry;
 
     public static void PastryCalc(int qty)
     {
       PastryQuantity = qty;
-      
-      if (PastryQuantity % 3 == 0)
-      {
-        PastryPrice = (PastryQuantity / 3) * 5;
-      }
-      else
-      {
       PastryPrice = qty * 2;
+
+      if (PastryQuantity >= 3)
+      {
+      groupedPastry = (qty / 3);
+      individualPastry = (qty % 3);
+      PastryPrice = (groupedPastry * 5) + (individualPastry * 2); 
       }
     }
   }
